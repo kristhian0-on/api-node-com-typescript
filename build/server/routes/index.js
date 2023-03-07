724ade57f3e16e2410255985770ac7bd8cec0d8f
Cristiano <kristian0tj2019@gmail.com>
2023-03-07 18:41:09 -0300

    feat: environment variables and convert ts to js

---
+++ build/server/routes/index.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.router = void 0;
+const express_1 = require("express");
+const http_status_codes_1 = require("http-status-codes");
+const router = (0, express_1.Router)();
+exports.router = router;
+router.get("/", (_, res) => {
+    return res.send('Olá DEV!');
+});
+router.get("/teste", (_, res) => {
+    return res.send('Olá DEV! Estamos testando...');
+});
+router.post("/teste", (req, res) => {
+    console.log(req.body);
+    return res.status(http_status_codes_1.StatusCodes.OK).json(req.body);
+});
--- src/server/Server.ts
+++ src/server/Server.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import { router } from "./routes";
 
+import "dotenv/config";
+
 const server = express();
 
 server.use(express.json());
