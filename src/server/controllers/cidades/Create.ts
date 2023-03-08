577ef7edd170d69da87045eec6dcc1db7203e0d4
Cristiano <kristian0tj2019@gmail.com>
2023-03-08 12:47:25 -0300

    feat: validations with YUP

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -1,11 +1,30 @@
 import { Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
+import * as yup from "yup";
 
 interface ICidade {
     nome: string;
 }
-export const create = (req: Request, res: Response) => {
 
-    const data: ICidade = req.body;
-    console.log(data.nome);
+const bodyValidation: yup.Schema<ICidade> = yup.object().shape({
+    nome: yup.string().required().min(3),
+});
+
+export const create = async (req: Request<{}, {}, ICidade>, res: Response) => {
+    let validateData: ICidade | undefined = undefined;
+
+    try {
+        validateData = await bodyValidation.validate(req.body);
+    } catch (error) {
+        const yupError = error as yup.ValidationError;
+        return res.json({
+            error: {
+                default: yupError.message,
+            }
+        });
+    };
+
+    console.log(validateData);
+    
     return res.send('Create!');
 };
