ea58e01c2f6e95532c6d38908e29e5805b67d89e
Cristiano <kristian0tj2019@gmail.com>
2023-03-08 13:41:22 -0300

    feat: update validations with YUP

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -4,27 +4,29 @@ import * as yup from "yup";
 
 interface ICidade {
     nome: string;
+    estado: string;
 }
 
 const bodyValidation: yup.Schema<ICidade> = yup.object().shape({
     nome: yup.string().required().min(3),
+    estado: yup.string().required().min(3),
 });
 
 export const create = async (req: Request<{}, {}, ICidade>, res: Response) => {
     let validateData: ICidade | undefined = undefined;
-
     try {
-        validateData = await bodyValidation.validate(req.body);
-    } catch (error) {
-        const yupError = error as yup.ValidationError;
-        return res.json({
-            error: {
-                default: yupError.message,
-            }
-        });
-    };
+        validateData = await bodyValidation.validate(req.body, { abortEarly: false });
+    } catch (err) {
+        const yupError = err as yup.ValidationError;
+        const errors: Record<string, string> = {};
 
+        yupError.inner.forEach(error => {
+            if (error.path === undefined) return;
+            errors[error.path] = error.message;
+        });
+
+        return res.status(StatusCodes.BAD_REQUEST).json({ errors });
+    };
     console.log(validateData);
-    
     return res.send('Create!');
 };
