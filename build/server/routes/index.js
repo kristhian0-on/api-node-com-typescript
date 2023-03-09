f0ac9b323af269325b22ea26edcd1cd835e6f89f
Cristiano <kristian0tj2019@gmail.com>
2023-03-09 12:30:10 -0300

    feat: configured Jest setup

--- build/server/routes/index.js
+++ build/server/routes/index.js
@@ -3,15 +3,14 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.router = void 0;
 const express_1 = require("express");
 const http_status_codes_1 = require("http-status-codes");
+const controllers_1 = require("./../controllers");
 const router = (0, express_1.Router)();
 exports.router = router;
 router.get("/", (_, res) => {
-    return res.send('Olá DEV!');
+    return res.status(http_status_codes_1.StatusCodes.OK).send('Olá DEV!');
 });
-router.get("/teste", (_, res) => {
-    return res.send('Olá DEV! Estamos testando...');
-});
-router.post("/teste", (req, res) => {
-    console.log(req.body);
-    return res.status(http_status_codes_1.StatusCodes.OK).json(req.body);
-});
+router.get("/cidades", controllers_1.CidadesController.getAllValidation, controllers_1.CidadesController.getAll);
+router.get("/cidades/:id", controllers_1.CidadesController.getByIdValidation, controllers_1.CidadesController.getById);
+router.put("/cidades/:id", controllers_1.CidadesController.updateByIdValidation, controllers_1.CidadesController.updateById);
+router.post("/cidades", controllers_1.CidadesController.createValidation, controllers_1.CidadesController.create);
+router.delete("/cidades/:id", controllers_1.CidadesController.deleteByIdValidation, controllers_1.CidadesController.deletetById);
